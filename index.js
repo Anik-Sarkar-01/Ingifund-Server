5002d286559cacd752f6e5e8760d4f19d951ebf7
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-03-04 20:40:36 +0600

    find a specific data using get added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,7 +34,16 @@ async function run() {
         })
 
         app.get("/campaigns", async(req, res) => {
-            
+            const cursor = campaignCollection.find().limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/campaign/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await campaignCollection.findOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
